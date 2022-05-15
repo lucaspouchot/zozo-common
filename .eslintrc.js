1351d364ac7a95369421aefda6d3d541201581b5
lucaspouchot <lucas.pouchot@espridigital.fr>
2022-05-15 16:12:55 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/indent': [
+      'error',
+      4,
+      {
+        'SwitchCase': 1,
+        'ignoredNodes': [
+          'FunctionExpression > .params[decorators.length > 0]',
+          'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
+          'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
+        ]
+      },
+    ],
+  },
+};
---
+++ src/logger/index.ts
@@ -0,0 +1,106 @@
+import { WinstonModule } from "nest-winston";
+import * as winston from "winston";
+import 'winston-daily-rotate-file';
+import safeStringify from "fast-safe-stringify";
+import { inspect } from "util";
+import * as chalk from "chalk";
+import { ensureString } from "../utils";
+
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    verbose: 4,
+    debug: 5,
+    silly: 6,
+};
+let verboseLevel = ensureString(process.env.VERBOSE_LVL).toLowerCase();
+if (!levels.hasOwnProperty(verboseLevel)) {
+    verboseLevel = 'verbose';
+}
+
+const nestLikeColorScheme: Record<string, chalk.Chalk> = {
+    error: chalk.red,
+    warn: chalk.yellow,
+    info: chalk.greenBright,
+    verbose: chalk.cyanBright,
+    debug: chalk.magentaBright,
+};
+
+export const logger = WinstonModule.createLogger({
+    levels: levels,
+    level: verboseLevel,
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                winston.format.printf(({ context, level, timestamp, message, ms, jwt, body: _, ...meta }) => {
+                    if ('undefined' !== typeof timestamp) {
+                        // Only format the timestamp to a locale representation if it's ISO 8601 format. Any format
+                        // that is not a valid date string will throw, just ignore it (it will be printed as-is).
+                        try {
+                            if (timestamp === new Date(timestamp).toISOString()) {
+                                timestamp = new Date(timestamp).toLocaleString();
+                            }
+                        } catch (error) {
+                            // eslint-disable-next-line no-empty
+                        }
+                    }
+
+                    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                    const color = nestLikeColorScheme[level] || ((text: string): string => text);
+
+                    if (jwt && jwt.login) {
+                        meta.user = jwt.login;
+                    }
+
+                    const stringifiedMeta = safeStringify(meta);
+                    const formattedMeta = inspect(JSON.parse(stringifiedMeta), { colors: true, depth: null, breakLength: Infinity });
+
+                    return (
+                        `${color('[FFKMDA]')} ` +
+                        `${chalk.yellow(level.charAt(0).toUpperCase() + level.slice(1))}\t` +
+                        ('undefined' !== typeof timestamp ? `${timestamp} ` : '') +
+                        ('undefined' !== typeof context
+                            ? `${chalk.yellow('[' + context + ']')} `
+                            : '') +
+                        `${color(message)} - ` +
+                        `${formattedMeta}` +
+                        ('undefined' !== typeof ms ? ` ${chalk.yellow(ms)}` : '')
+                    );
+                })
+            ),
+        }),
+        new winston.transports.DailyRotateFile({
+            json: true,
+            maxFiles: '8d',
+            dirname: 'log',
+            datePattern: 'YYYY-MM-DD',
+            filename: '%DATE%.ffkmda.jsonl',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                winston.format.printf(info => {
+                    const { context, level, timestamp, message, ms, ...meta } = info;
+                    let formattedMeta = safeStringify(meta);
+                    try {
+                        formattedMeta = JSON.parse(formattedMeta);
+                    }
+                    catch(e) {}
+                    return JSON.stringify({
+                        timestamp: new Date(timestamp).getTime(),
+                        level: level,
+                        date: new Date(timestamp).toISOString(),
+                        context: context,
+                        delay: ms,
+                        message: message,
+                        data: formattedMeta,
+                    });
+                }),
+            ),
+        }),
+    ],
+});
